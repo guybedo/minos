4902fcaf655183e5437d56b0beaf595d2f247c46
julien <rochjulien@yahoo.fr>
2017-02-19 21:58:13 -0800

    comment examples

--- examples/ga/search_model_layout_and_params.py
+++ examples/ga/search_model_layout_and_params.py
@@ -3,34 +3,94 @@ Created on Feb 6, 2017
 
 @author: julien
 '''
-from minos.experiment.blueprint import Blueprint
-from minos.model.parameters.layout import Layout
-from minos.model.parameters.metric import Metric
-from minos.model.parameters.model import ModelParameters
-from minos.model.parameters.objective import Objective
-from minos.model.parameters.optimizer import Optimizer,\
-    OptimizerParameters
-from minos.model.parameters.training import Training,\
-    MetricDecreaseStoppingCondition
+from minos.experiment.experiment import Experiment, ExperimentParameters,\
+    load_experiment_blueprints
+from minos.experiment.ga import run_ga_search_experiment
+from minos.experiment.training import Training, AccuracyDecreaseStoppingCondition,\
+    EpochStoppingCondition
+from minos.model.build import ModelBuilder
+from minos.model.model import Objective, Optimizer, Metric, Layout
+from minos.model.parameter import int_param, string_param, float_param
+from minos.train.utils import CpuEnvironment, cpu_device, Environment
+
+from examples.ga.dataset import get_reuters_dataset
+import numpy as np
 
 
-def search():
-    input_size = 10
-    output_size = 2
-    train, test = None, None
-    batch_size = 1000
-    epoch_steps = int(len(train) / batch_size)
+np.random.seed(1337)
+max_words = 1000
 
-    layout = Layout(
-        layout_parameters=LayoutParameters(),
-        model_parameters=ModelParameters())
+
+def build_layout(input_size, output_size):
+    return Layout(
+        input_size=input_size,
+        output_size=output_size,
+        output_activation='softmax')
+
+
+def custom_experiment_parameters():
+    experiment_parameters = ExperimentParameters()
+    experiment_parameters.layout_parameter('blocks', int_param(1, 5))
+    experiment_parameters.layout_parameter('layers', int_param(1, 5))
+    experiment_parameters.layer_parameter('Dense.output_dim', int_param(10, 500))
+    experiment_parameters.layer_parameter('Dense.activation', string_param(['relu', 'tanh']))
+    experiment_parameters.layer_parameter('Dropout.p', float_param(0.1, 0.9))
+    return experiment_parameters
+
+
+def metric_decrease_stopping_condition():
+    return AccuracyDecreaseStoppingCondition(
+        min_epoch=2,
+        max_epoch=10,
+        noprogress_count=5,
+        measurement_interval=0.25)
+
+
+def epoch_stopping_condition():
+    return EpochStoppingCondition(epoch=10)
+
+
+def search_model(experiment_label, steps, batch_size=32):
+    batch_iterator, test_batch_iterator, nb_classes = get_reuters_dataset(batch_size, max_words)
+    layout = build_layout(max_words, nb_classes)
     training = Training(
-        Objective('mean_squared_error'),
-        Optimizer(parameters=OptimizerParameters()),
-        Metric('binary_accuracy'),
-        MetricDecreaseStoppingCondition(
-            min_step_count=epoch_steps,
-            max_step_count=25 * epoch_steps,
-            flat_step_count=3,
-            measurement_interval=epoch_steps))
-    return Blueprint(layout, training)
+        Objective('categorical_crossentropy'),
+        Optimizer(optimizer='Adam'),
+        Metric('categorical_accuracy'),
+        epoch_stopping_condition(),
+        batch_size)
+    parameters = custom_experiment_parameters()
+    experiment = Experiment(
+        experiment_label,
+        layout,
+        training,
+        batch_iterator,
+        test_batch_iterator,
+        CpuEnvironment(n_jobs=2),
+        parameters=parameters)
+    run_ga_search_experiment(
+        experiment,
+        population_size=100,
+        generations=steps,
+        resume=False)
+
+
+def load_best_model(experiment_label, step):
+    blueprints = load_experiment_blueprints(
+        experiment_label,
+        step,
+        Environment())
+    return ModelBuilder().build(
+        blueprints[0],
+        cpu_device(),
+        compile_model=False)
+
+
+def main():
+    experiment_label = 'reuters_experiment'
+    steps = 100
+    search_model(experiment_label, steps)
+    load_best_model(experiment_label, steps - 1)
+
+if __name__ == '__main__':
+    main()
--- examples/ga/search_model_params.py
+++ examples/ga/search_model_params.py
@@ -3,20 +3,17 @@ Created on Feb 6, 2017
 
 @author: julien
 '''
-from keras.datasets import reuters
-from keras.preprocessing.text import Tokenizer
-from keras.utils import np_utils
 from minos.experiment.experiment import Experiment, ExperimentParameters,\
     load_experiment_blueprints
 from minos.experiment.ga import run_ga_search_experiment
-from minos.experiment.training import Training, MetricDecreaseStoppingCondition,\
+from minos.experiment.training import Training, AccuracyDecreaseStoppingCondition,\
     EpochStoppingCondition
 from minos.model.build import ModelBuilder
 from minos.model.model import Objective, Optimizer, Metric, Layout
 from minos.model.parameter import int_param, string_param, float_param
-from minos.train.utils import CpuEnvironment, SimpleBatchIterator, cpu_device,\
-    Environment
+from minos.train.utils import CpuEnvironment, cpu_device, Environment
 
+from examples.ga.dataset import get_reuters_dataset
 import numpy as np
 
 
@@ -24,29 +21,10 @@ np.random.seed(1337)
 max_words = 1000
 
 
-def get_dataset(batch_size):
-    (X_train, y_train), (X_test, y_test) = reuters.load_data(nb_words=max_words, test_split=0.2)
-    nb_classes = np.max(y_train) + 1
-    tokenizer = Tokenizer(nb_words=max_words)
-    X_train = tokenizer.sequences_to_matrix(X_train, mode='binary')
-    X_test = tokenizer.sequences_to_matrix(X_test, mode='binary')
-    y_train = np_utils.to_categorical(y_train, nb_classes)
-    y_test = np_utils.to_categorical(y_test, nb_classes)
-
-    batch_iterator = SimpleBatchIterator(
-        X_train,
-        y_train,
-        batch_size,
-        autoloop=True)
-    test_batch_iterator = SimpleBatchIterator(
-        X_test,
-        y_test,
-        len(X_test),
-        autoloop=True)
-    return batch_iterator, test_batch_iterator, nb_classes
-
-
 def build_layout(input_size, output_size):
+    """ Here we define a minimal layout. We don't specify the architecture.
+    Layouts will be randomly generated using the min and max numbers of rows, blocks and layers
+    """
     return Layout(
         input_size=input_size,
         output_size=output_size,
@@ -54,7 +32,20 @@ def build_layout(input_size, output_size):
 
 
 def custom_experiment_parameters():
-    experiment_parameters = ExperimentParameters()
+    """ Here we define the exeriment parameters.
+    We are using use_default_values=True, which will initialize
+    all the parameters with their default values. These parameters are then fixed
+    for the duration of the experiment and won't evolve.
+    That means that we need to manually specify which parametres we want to test,
+    and the possible values, either intervals or lists of values.
+
+    If we want to test all the parameters and possible values, we can
+    set use_default_values to False. In that case, random values will be generated
+    and tested during the experiment. We can redefine some parameters if we want to
+    fix their values.
+    Reference parameters and default values are defined in minos.model.parameters
+    """
+    experiment_parameters = ExperimentParameters(use_default_values=True)
     experiment_parameters.layout_parameter('blocks', int_param(1, 5))
     experiment_parameters.layout_parameter('layers', int_param(1, 5))
     experiment_parameters.layer_parameter('Dense.output_dim', int_param(10, 500))
@@ -63,20 +54,31 @@ def custom_experiment_parameters():
     return experiment_parameters
 
 
-def metric_decrease_stopping_condition():
-    return MetricDecreaseStoppingCondition(
+def accuracy_decrease_stopping_condition():
+    """ This stopping condition lets the training continue as long as the
+    accuracy improves and stops if it doesn't improve for 5 epochs
+    """
+    return AccuracyDecreaseStoppingCondition(
         min_epoch=2,
         max_epoch=10,
-        noprogress_count=5,
-        measurement_interval=0.25)
+        noprogress_count=5)
 
 
 def epoch_stopping_condition():
+    """ This stopping condition lets the training run for 10 epochs
+    """
     return EpochStoppingCondition(epoch=10)
 
 
 def search_model(experiment_label, steps, batch_size=32):
-    batch_iterator, test_batch_iterator, nb_classes = get_dataset(batch_size)
+    """ This is where we put everythin together.
+    We get the dataset, build the Training and Experiment objects, and run the experiment.
+    The experiments logs are generated in ~/minos/experiment_label
+    We use the CpuEnvironment to have the experiment run on the cpu, with 2 parralel processes.
+    We could use GpuEnvironment to use GPUs, and specify which GPUs to use, and how many tasks
+    per GPU
+    """
+    batch_iterator, test_batch_iterator, nb_classes = get_reuters_dataset(batch_size, max_words)
     layout = build_layout(max_words, nb_classes)
     training = Training(
         Objective('categorical_crossentropy'),
@@ -101,12 +103,15 @@ def search_model(experiment_label, steps, batch_size=32):
 
 
 def load_best_model(experiment_label, step):
+    """ Here we load the blueprints generated during an experiment
+    and create the Keras model from the top scoring blueprint
+    """
     blueprints = load_experiment_blueprints(
         experiment_label,
         step,
         Environment())
     return ModelBuilder().build(
-        blueprints[0], 
+        blueprints[0],
         cpu_device(),
         compile_model=False)
 
