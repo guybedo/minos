68ce70082f35a032aee6d9588db3310a5554705b
julien <rochjulien@yahoo.fr>
2017-02-25 15:32:23 -0800

    quiet logs, clear tf session

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 setup(name='pyminos',
-      version='0.2.1',
+      version='0.2.2',
       description='Neural nets hyper parameters & architecture search with genetic algorithms',
       keywords=['keras', 'genetic algorithm', 'neural network', 'deep learning'],
       author='Julien Roch',
