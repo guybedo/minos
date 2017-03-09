6c1ffe855e68f51b2ed8213b3bd8d83b5a93dd8f
julien <rochjulien@yahoo.fr>
2017-03-09 12:40:41 -0800

    fix imports

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='pyminos',
-      version='0.4.4',
+      version='0.4.5',
       description='Neural nets hyper parameters & architecture search with genetic algorithms',
       keywords=['keras', 'genetic algorithm', 'neural network', 'deep learning'],
       author='Julien Roch',
